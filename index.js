148c91a38e39c7020331b3a89db33fb3a5d41007
Talita Aya <tatakuriki@gmail.com>
2022-07-30 18:20:41 -0300

    new carousel + responsive carousel

--- index.js
+++ index.js
@@ -1,64 +1,96 @@
-function pageScroll() {
-  window.scrollTo(300, 650);
+//carousel
+var index = 0,
+    amount = 0,
+    currTransl = [],
+    translationComplete = true,
+    moveOffset = 0;
+
+var transitionCompleted = function(){
+    translationComplete = true;
 }
 
-var balls = document.querySelector(".balls");
-var quant = document.querySelectorAll(".slides .page");
-var atual = 0;
-var page = document.getElementById("atual");
-var next = document.getElementById("next");
-var back = document.getElementById("back");
+document.addEventListener("DOMContentLoaded", function(event) 
+{
+    var carousel = document.getElementById('carousel');
 
-for (let i = 0; i < quant.length; i++) {
-  var div = document.createElement("div");
-  div.id = i;
-  balls.appendChild(div);
-}
-
-document.getElementById("0").classList.add("imgAtual");
-
-var pos = document.querySelectorAll(".balls div");
-
-for (let i = 0; i < pos.length; i++) {
-  pos[i].addEventListener("click", () => {
-    atual = pos[i].id;
-    slide();
-  });
-}
-
-back.addEventListener("click", () => {
-  atual--;
-  slide();
+    amount = document.getElementsByClassName("slide").length;
+    // get the width of the container
+    moveOffset = parseInt(window.getComputedStyle(document.getElementById('carousel-container')).width, 10);
+    // calcuate the width of the carousel
+    document.getElementById('carousel').style.width = (amount * moveOffset) + 'px';
+    // prevent multiple click when transition
+    for(var i = 0; i < amount; i++)
+    {
+        currTransl[i] = -moveOffset;
+        document.getElementsByClassName("slide")[i].addEventListener("transitionend", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("webkitTransitionEnd", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("oTransitionEnd", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("MSTransitionEnd", transitionCompleted, true);                  
+    }
+    // add the last item to the start so that translateX(-moveOffset) works (In case the first click is the previous button)
+    document.getElementById('carousel').insertBefore(document.getElementById('carousel').children[4], document.getElementById('carousel').children[0])
+    // add click events to control arrows
+    document.getElementById('prev').addEventListener('click', prev, true);
+    document.getElementById('next').addEventListener('click', next, true);
 });
 
-next.addEventListener("click", () => {
-  atual++;
-  slide();
-});
-
-function slide() {
-  if (atual >= quant.length) {
-    atual = 0;
-  } else if (atual < 0) {
-    atual = quant.length - 1;
-  }
-  document.querySelector(".imgAtual").classList.remove("imgAtual");
-  page.style.marginLeft = -1110 * atual + "px";
-  document.getElementById(atual).classList.add("imgAtual");
+function prev()
+{
+    if (translationComplete === true)
+    {
+        translationComplete = false;
+        index--;
+        if (index == -1)
+        {
+            index = amount-1;
+        }
+        var outerIndex = (index) % amount;
+        for (var i = 0; i < amount; i++)
+        {
+            var slide = document.getElementsByClassName("slide")[i];    
+            slide.style.opacity = '1';    
+            slide.style.transform = 'translateX('+(currTransl[i]+moveOffset)+'px)';
+            currTransl[i] = currTransl[i]+moveOffset;
+        }
+        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+        outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]-(moveOffset*amount))+'px)';
+        outerSlide.style.opacity = '0';
+        currTransl[outerIndex] = currTransl[outerIndex]-moveOffset*(amount);
+    }
 }
 
+function next()
+{
+    if (translationComplete === true)
+    {
+        translationComplete = false;
+        var outerIndex = (index) % amount;
+        index++;
+        for(var i = 0; i < amount; i++)
+        {
+            var slide = document.getElementsByClassName("slide")[i];    
+            slide.style.opacity = '1';    
+            slide.style.transform = 'translateX('+(currTransl[i]-moveOffset)+'px)';
+            currTransl[i] = currTransl[i]-moveOffset;
+        }
+        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+        outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]+(moveOffset*amount))+'px)';
+        outerSlide.style.opacity = '0';
+        currTransl[outerIndex] = currTransl[outerIndex]+moveOffset*(amount);
+    }
+}
 
 
 // responsividade menu mobile
-const menu = document.getElementById("btn-mobile")
-menu.addEventListener('click', toggleMenu)
+const menu = document.getElementById("btn-mobile");
+menu.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  console.log('oi')
-  menu.classList.toggle('close')
-  const menuExpand = document.getElementById("menu-list")
-  const langExpand = document.getElementById("language")
+  console.log("oi");
+  menu.classList.toggle("close");
+  const menuExpand = document.getElementById("menu-list");
+  const langExpand = document.getElementById("language");
 
-  menuExpand.classList.toggle('active')
-  langExpand.classList.toggle('active')
+  menuExpand.classList.toggle("active");
+  langExpand.classList.toggle("active");
 }
